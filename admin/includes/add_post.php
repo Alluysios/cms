734d90aca0fe3ae07f6e688f1e327a70f0940b07
Alluysios <arribaallu@gmail.com>
2019-01-23 17:40:26 -0700

    cleaning up & adding widget status
    graph not working
    TODO: adding php code in script tag.

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -26,8 +26,7 @@
         confirm($create_post_query);
     }
 ?>
-<form action="" method="post" enctype="multipart/form-data">    
-     
+<form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
         <input type="text" class="form-control" name="title">
